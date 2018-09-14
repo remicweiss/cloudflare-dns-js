517a29837f2c404a1f0f6109a6ae89b962f08cdf
remicweiss <remicweiss@users.noreply.github.com>
2018-09-14 16:56:50 +0200

    Update cloudflare-dns.js
    Fixed formatting and bad code. More compatibility with older browsers

--- cloudflare-dns.js
+++ cloudflare-dns.js
@@ -1,38 +1,26 @@
-function resolve(what,type)
-{
-    return new Promise(resolve => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                resolve(JSON.parse(this.responseText));
-            }
-            else if (this.readyState === 4 && this.status !== 200) {
-                resolve({Status: -1, requestStatus: this.status});
-            }
-        };
-        xhttp.open("GET", "https://cloudflare-dns.com/dns-query?name="+what+"&type="+type, true);
-        xhttp.setRequestHeader("accept",'application/dns-json');
-        xhttp.send();
-    });
-}
+class CloudflareDns {
 
-function getIP(what,type)
-{
-    return new Promise(resolve => {
+    static resolve(domain, type, response) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                if(typeof (JSON.parse(this.responseText).Answer) === "undefined")
-                    resolve(null);
-                else
-                    resolve(JSON.parse(this.responseText).Answer[0].data);
-            }
-            else if (this.readyState === 4 && this.status !== 200) {
-                resolve({Status: -1, requestStatus: this.status});
+            if (this.readyState === 4) {
+                response(this.status === 200 ? JSON.parse(this.responseText) : { Status: -1, requestStatus: this.status });
             }
         };
-        xhttp.open("GET", "https://cloudflare-dns.com/dns-query?name="+what+"&type="+type, true);
-        xhttp.setRequestHeader("accept",'application/dns-json');
+        xhttp.open("GET", "https://cloudflare-dns.com/dns-query?name=" + domain + "&type=" + type, true);
+        xhttp.setRequestHeader("accept", 'application/dns-json');
         xhttp.send();
-    });
+    }
+
+    static getIPv4(domain, response) {
+
+        function interResolve(data) { (typeof (data.Answer) === "undefined" ? response(null) : response(data.Answer[0].data)) }
+        this.resolve(domain, "A", interResolve);
+    }
+
+    static getIPv6(domain, response) {
+
+        function interResolve(data) { (typeof (data.Answer) === "undefined" ? response(null) : response(data.Answer[0].data)) }
+        this.resolve(domain, "AAAA", interResolve);
+    }
 }
